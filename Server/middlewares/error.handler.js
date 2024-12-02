43ed2f3d3be2efc6efce239df25e3508c9205761
Johny Molano <molano.johny721@gmail.com>
2024-12-02 14:21:09 -0500

    middleware creado para manejo de errores.

---
+++ Server/middlewares/error.handler.js
@@ -0,0 +1,35 @@
+//Capturador de errores 
+function logError(error, req, res, next) {
+    console.log(`LogErrors`);
+    console.error(error);
+    next(error);
+};
+
+//Mostrar el error en un formato legible
+function errorHandler(error, req, res, next) {
+    console.log(`ErrorHandler`);
+    res.status(500).json({
+        message: error.message,
+        stack: error.stack
+    });
+    next();
+};
+
+//Errores tipo boom
+function boomErrorHandler(error, req, res, next) {
+    //validar si el error es creado por la libreria boom
+    if (err.isBoom) {
+        const { output } = err;
+    //estatus code dinamico y json leidos desde el output de boom
+        res.status(output.statusCode).json(output.payload);
+    } else {
+    //si no es un errore de tipo boom ira a ejecutar un middleware de errores normales
+        next(err);
+    }
+};
+
+module.exports = {
+    logError,
+    errorHandler,
+    boomErrorHandler
+};
