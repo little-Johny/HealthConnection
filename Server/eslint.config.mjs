a2947206fb4878b4c9d3032fb725e63cd7910455
little-johny <molano.johny721@gmail.com>
2025-03-22 20:23:45 -0500

    chore(lint): update ESLint and Prettier rules

--- Server/eslint.config.mjs
+++ Server/eslint.config.mjs
@@ -1,5 +1,6 @@
-import js from '@eslint/js'
-import globals from 'globals'
+import js from '@eslint/js';
+import globals from 'globals';
+import prettier from 'eslint-plugin-prettier';
 
 export default [
   { ignores: ['dist', 'node_modules', 'build'] }, // Ignorar archivos innecesarios
@@ -11,9 +12,19 @@ export default [
       sourceType: 'commonjs', // Backend suele usar CommonJS
       globals: globals.node, // Variables globales de Node.js
     },
+    plugins: {
+      prettier,
+    },
+    extends: ['plugin:prettier/recommended'], // Activa Prettier automáticamente
     rules: {
       ...js.configs.recommended.rules,
+      'prettier/prettier': [
+        'error',
+        {
+          singleQuote: true,
+          endOfLine: 'lf', // LF para evitar problemas de compatibilidad
+        },
+      ],
     },
   },
-
-]
+];
