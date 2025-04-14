bf36a5c2650406e79688e82a9ac0ea903628735c
little-johny <molano.johny721@gmail.com>
2025-04-13 21:21:28 -0500

    feat(api-client): Use axios for communication between API and Client(frontend), add setting file, auth file and user file

---
+++ Client/src/api/auth.js
@@ -0,0 +1,5 @@
+import api from './api';
+
+export const login = (credentials) => api.post('/auth/login', credentials);
+export const recovery = (email) => api.post('/auth/recovery', email);
+export const changePassword = (data) => api.post('/auth/change-password', data) ;
