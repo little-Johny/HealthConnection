69568a28851ff7c0aa53f3caf51195b2a2749165
Johny Molano <molano.johny721@gmail.com>
2025-05-22 13:14:22 -0500

    docs(process-map): Add and fix process map

--- Client/src/api/api.js
+++ Client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: `http://172.18.65.151:3000/health_connection/v2`,
+    baseURL: `http://localhost:3000/health_connection/v2`,
     headers: {
         'Content-Type': 'application/json',
     },
